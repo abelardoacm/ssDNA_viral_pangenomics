bb8eaaa7142b9da4f38f915e3d7df1906484ed35
abelardoacm <abelardoacm@ciencias.unam.mx>
2020-11-13 02:42:45 -0600

    New script for protein count filtering based on used defined interval and counts from reference values. Pending to add usage in README.md

--- bin/3_Protein_count_filtering.pl
+++ bin/3_Protein_count_filtering.pl
@@ -4,13 +4,20 @@
 #The first positional argument corresponds to the family.
 $familia = $ARGV[0];
 $carpetafamilia = "$familia"."_fasta_proteomes";
-system ("grep -c \">\" ../data/Proteomic_fasta_files/$carpetafamilia/* > PROTEIN_COUNT_$familia");
+use POSIX;
+system ("cp -r ../data/Genomic_fasta_files/$familia\_concatenated_fasta_genomes .");
+system ("cp -r ../data/Individual_full_genbank_files/$familia\_concatenated_genbank_genomes .");
+system ("cp -r ../data/Proteomic_fasta_files/$familia\_concatenated_fasta_proteomes .");
+system ("mv $familia\_concatenated_fasta_genomes $familia\_catfiltered_fasta_genomes ");
+system ("mv $familia\_concatenated_genbank_genomes $familia\_catfiltered_genbank_genomes");
+system ("mv $familia\_concatenated_fasta_proteomes $familia\_catfiltered_fasta_proteomes");
+system ("grep -c \">\" $familia\_catfiltered_fasta_proteomes/* > PROTEIN_COUNT_$familia");
+
 open (FILE, "PROTEIN_COUNT_$familia");
-open (OUT, ">>PC_rejected_list");
 $numero_proteomas = 0;
 $suma_conteo = 0;
 
-#Once the protein_count_famil file is opened, limit values are set. To the moment, a 95% difference from the mean is allowed
+#Once the protein_count_famil file is opened, limit values are set
 while ($linea = <FILE>) {
 	chomp ($linea);
 	my ( $count ) = $linea=~ /(\:.*)\s*$/;
@@ -19,14 +26,15 @@ while ($linea = <FILE>) {
 	$numero_proteomas += 1;	
 }
 
+
 #The second positional argument corresponds to the allowed percentage of difference from the mean count
 
 $percdiff = $ARGV[1];
 $diff = $percdiff / 100;
-$promedio_conteo = $suma_conteo / $numero_proteomas;
+$promedio_conteo = ceil($suma_conteo / $numero_proteomas);
 $intervalo = $promedio_conteo * $diff;
-$maxcalc = $promedio_conteo + $intervalo;
-$mincalc = $promedio_conteo - $intervalo;
+$maxcalc = ceil($promedio_conteo + $intervalo);
+$mincalc = ceil($promedio_conteo - $intervalo);
 
 
 #The third and fourth positional argument correspond to the minimum and maximum protein count for reference strains. Such info can be easily obtained from Viralzone.
@@ -36,28 +44,32 @@ $maxref = $ARGV[3];
 
 if ($minref == 0) {
 	$minref = $mincalc;
+	print STDOUT "No minimum protein count from reference proteome was given... setting lower limit to -\%$percdiff deviation from the mean \n";
 }
 
 if ($maxref == 0) {
 	$maxref = $maxcalc;
+	print STDOUT "No maximum protein count from reference proteome was given... setting upper limit to +\%$percdiff deviation from the mean \n\n";
 }
 
-
+print STDOUT "rounded-up mean protein count is $promedio_conteo \n\n";
 
 if ($minref < $mincalc) {
 	$lim_inferior = $minref;
+	print STDOUT "Minimum protein count from reference strain is lower than -\%$percdiff cutoff value... setting lower limit to $minref proteins \n";
 }else{
 	$lim_inferior = $mincalc;
+	print STDOUT "... setting lower protein cutoff count in $lim_inferior proteins \n";
 }
 
 
 if ($maxref > $maxcalc) {
 	$lim_superior = $maxref;
+	print STDOUT "Maximum protein count from reference strain is bigger than +\%$percdiff cutoff value... setting upper limit to $maxref proteins \n\n";
 }else{
 	$lim_superior = $maxcalc;
+	print STDOUT "... setting upper protein cutoff count in $lim_superior proteins \n\n";
 }
-
-
 
 
 
@@ -69,9 +81,20 @@ while ($linea = <FILE>) {
 	$count =~ s/://g;
 	if ($count > $lim_superior || $count < $lim_inferior ) {
 		my $rejected_file = substr($linea, 0, index($linea, ":"));
-		print STDOUT "$rejected_file will be filtered as protein count is $count \n";
-		print OUT "$rejected_file\n";
+		my $rejwithoutextension = substr($rejected_file, 0, index($linea, "."));
+		my $pathtofile = "$familia\_catfiltered_fasta_proteomes/";
+		$rejwithoutpath = $rejwithoutextension;
+		$rejwithoutpath =~ s/$pathtofile//g;
+		print STDOUT "$rejwithoutpath will be filtered as protein count is $count \n";
+		system ("rm $familia\_catfiltered_fasta_genomes/$rejwithoutpath*");
+		system ("rm $familia\_catfiltered_genbank_genomes/$rejwithoutpath*");
+		system ("rm $familia\_catfiltered_fasta_proteomes/$rejwithoutpath*");
 	}
 }
-close (FILE);
 system ("rm PROTEIN_COUNT_$familia");
+system ("mv $familia\_catfiltered_fasta_genomes ../data/Genomic_fasta_files/");
+system ("mv $familia\_catfiltered_genbank_genomes ../data/Individual_full_genbank_files/");
+system ("mv $familia\_catfiltered_fasta_proteomes ../data/Proteomic_fasta_files/");
+
+
+
