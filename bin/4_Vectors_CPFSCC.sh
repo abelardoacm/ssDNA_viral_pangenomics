d25ae16708a3c86715c1798cd53d1a835a45c024
abelardoacm <abelardoacm@ciencias.unam.mx>
2020-12-02 01:34:33 -0600

    few extra lines in script 4 to clean fasta headers of CPFSCC.m input

--- bin/4_Vectors_CPFSCC.sh
+++ bin/4_Vectors_CPFSCC.sh
@@ -5,6 +5,10 @@ mkdir ../data/AF_methods_input
 #A positional argument $1 is expected when calling this script. (i.e. ./4_Get_AF_methods_input.sh Geminiviridae.gb)
 
 awk ' /^>/ && FNR > 1 {next} {print $0} ' ../data/Genomic_fasta_files/$1_catfiltered_fasta_genomes/* > ../data/AF_methods_input/$1_AF_input.fasta
+sed -i 's/\//-/g' ../data/AF_methods_input/$1_AF_input.fasta
+sed -i 's/,/-/g' ../data/AF_methods_input/$1_AF_input.fasta
+sed -i 's/;/-/g' ../data/AF_methods_input/$1_AF_input.fasta
+sed -i 's/:/-/g' ../data/AF_methods_input/$1_AF_input.fasta
 cp ../data/AF_methods_input/$1_AF_input.fasta input.fasta
 mkdir ../results
 mkdir ../results/Central_moments_and_covariance_vectors_CPFSCC
