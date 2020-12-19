a5dce69ad1cbb47595b10c7b740dfc45825f69a2
abelardoacm <abelardoacm@ciencias.unam.mx>
2020-12-19 00:54:08 -0600

    Minor change in argument to prevent stdout warning message

--- bin/6_Files_to_clusters.sh
+++ bin/6_Files_to_clusters.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir ../results/Pangenomic_input_clusters
-mkdir $1_clusters
+mkdir -p ../results/Pangenomic_input_clusters
+mkdir -p $1_clusters
 cp ../data/Individual_full_genbank_files/$1_catfiltered_genbank_genomes/* .
 cat ../results/NbClust_membership_vectors/$1_membership_vectors.csv | grep -v -i "consenso" | cut -d, -f1,10 | sed 's/"//g' | sed 's/,/.gbk,cluster_/g' | sed 's/,/ /g' | sed 's/taxid/*taxid/g' > rename_rules.txt
 cat ../results/NbClust_membership_vectors/$1_membership_vectors.csv | sed 's/"//g'| cut -d, -f10 | sort | uniq | grep -v -i "consenso" | while read line ; do
