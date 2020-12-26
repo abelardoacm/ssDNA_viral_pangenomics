33add9a7f5ebed4ba52cc2929292601bc54a8647
abelardoacm <abelardoacm@ciencias.unam.mx>
2020-12-26 01:08:41 -0600

    Delayed one position in workflow

---
+++ bin/7_Files_to_clusters.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p ../results/Pangenomic_input_clusters
+mkdir -p $1_clusters
+cp ../data/Individual_full_genbank_files/$1_catfiltered_genbank_genomes/* .
+cat ../results/NbClust_membership_vectors/$1_membership_vectors.csv | grep -v -i "consenso" | cut -d, -f1,10 | sed 's/"//g' | sed 's/,/.gbk,cluster_/g' | sed 's/,/ /g' | sed 's/taxid/*taxid/g' > rename_rules.txt
+cat ../results/NbClust_membership_vectors/$1_membership_vectors.csv | sed 's/"//g'| cut -d, -f10 | sort | uniq | grep -v -i "consenso" | while read line ; do
+   mkdir cluster_$line
+done
+eval "$(sed 's/^/mv /g' rename_rules.txt )"
+mv cluster* $1_clusters
+mv $1_clusters ../results/Pangenomic_input_clusters/
+rm rename_rules.txt
+rm *.gbk
