fc68ee7041c04f78a4a9982b9b377412d0fa1e7d
abelardoacm <abelardoacm@ciencias.unam.mx>
2021-04-28 01:38:30 -0500

    Script being developed, current goal is to generate a phylo network. At its current state the script does not work with user input and does not writes networks. For usage and changes its meant to be displayed using Rstudio

--- bin/5ma_NbClust.r
+++ bin/5ma_NbClust.r
@@ -26,11 +26,12 @@ suppressPackageStartupMessages(library("factoextra"))
 suppressPackageStartupMessages(library("ggplot2"))
 suppressPackageStartupMessages(library("dplyr"))
 suppressPackageStartupMessages(library("tidyr"))
+suppressPackageStartupMessages(library("igraph"))
 #################################################
 # Reading input
 setwd("../results/Distance_Matrices") # mashdistmx location
 PosArgs <- as.character(commandArgs(trailingOnly = TRUE))
-family = "Circoviridae"
+#family = "Circoviridae"
 family = PosArgs[1]  #Reads positional arguments to define family (e.g. family = "Geminiviridae")
 mincluster = as.numeric(PosArgs[2]) # defines minimum number of clusters
 maxcluster = as.numeric(PosArgs[3]) # defines maximum number of clusters
@@ -61,6 +62,7 @@ nc <- names(sort(summary(as.factor(Bestnc$Best_num_clust)), decreasing = TRUE)[1
 ncsubset <- subset(Bestnc, Best_num_clust==nc) #Defines a subset of those indexes indicating matching nc
 first_index <- rownames(ncsubset)[1] #Selects index to operate partitions
 #################################################
+partitions_list <- c("wardD", "wardD2", "single", "complete", "average", "mcquitty", "median", "centroid", "consensus")
 #Computing partitions
 #Can be changed to for loop but I prefer this way to easily find when a partition method aborts
 Partition_wardD <- NbClust(diss=dist, distance = NULL, min.nc=mincluster, max.nc=maxcluster, method = "ward.D", index = first_index)
@@ -108,7 +110,9 @@ SumDist <- (rowSums(numericdist)) #get sum of distances by genome
 SumDist.df <- as.data.frame(SumDist) #transform to data frame
 mean_Distance <- (SumDist.df$SumDist)/ene #get average sum of distances per genome
 SumDist.df$MeanDif <- mean_Distance #save average sum to data frame
-SumDist.df$Membership <- as.factor(Pertenencia.df$Consenso)
+##Este es el bloque de codigo a corregir
+colnames(Pertenencia.df) <- partitions_list
+SumDist.df <- cbind(SumDist.df,Pertenencia.df)
 OrderedDist.df <- SumDist.df[order(SumDist.df$MeanDif),] #sort sum of distances
 IndexByDistSum <- seq(from = 1, to = ene)
 OrderedDist.df$IndexByDistSum <- IndexByDistSum
@@ -118,49 +122,88 @@ outdir3 <- ("mkdir -p Clustering_graphics") #make clustering graphics outdir
 system(outdir3)
 legendtitle <- paste(family, "ordered distances by cluster")
 setwd("Clustering_graphics")
-# ggplot pplot in tiff file
-tiff(outfile3, units="in", width=7, height=5, res=600) #tiff resolution parameters
-ggplot(data=OrderedDist.df, aes(IndexByDistSum, MeanDif)) +
-  geom_point(aes(colour = Membership), size = 2, alpha = 0.2) +
-  theme(legend.position="top") +
-  labs(color= legendtitle )
-dev.off()
 #################################################
-#Clusters PCA
-##### CHECAR LA PAGINA QUE DEJO ABIERTA DE INTERNET PARA HACER UN PCA LIKE SOLO CON LAS DISTANCIAS
+##Linear point plots group by group
+outfile_pplot <- ""
+legendtitle <- ""
+
+for (i in 1:length(partitions_list)){
+  outfile_pplot <- paste(family, "_mash_distpplot_", partitions_list[i],"_method.tiff", sep = "")
+  legendtitle <- paste(family, paste("(", partitions_list[i], ")", sep = ""),"ordered distances by cluster")
+  tiff(outfile_pplot, units="in", width=7, height=5, res=600)
+  print(ggplot(data=OrderedDist.df, aes(IndexByDistSum, MeanDif)) +
+    geom_point(aes(colour = as.factor(OrderedDist.df[[2+i]])), size = 2, alpha = 0.2) +
+    theme(legend.position="top") +
+    labs(color= legendtitle ))
+  dev.off()
+}
+
+#################################################
+#MDS
+#Esta ya vimos que funciona
+g <- graph.full(nrow(family_distmx))
+V(g)$label <- rownames(family_distmx)
+layout <- layout_with_mds(g, dist = as.matrix(family_distmx))
+plot(g, layout = layout, vertex.size = 4)
+# La grafica que se genera me gusta y creo que tiene potencial
+# Este libro es para echarle un ojo https://sites.fas.harvard.edu/~airoldi/pub/books/BookDraft-CsardiNepuszAiroldi2016.pdf
+# Ahi puedo ayudarme para explotar mejor el potencial de igraph
+# Estos dias lo que puedo hacer es intentar embelecer este grafico un poco mas 
+# Luego automatizar los pasos hasta llegar al producto
+#################################################
+#Intentar embelecer el gráfico
+net.bg <- graph.full(nrow(family_distmx))
+V(net.bg)$size <- 3
+V(net.bg)$frame.color <- "white"
+V(net.bg)$color <- "orange"
+V(net.bg)$label <- "" 
+E(net.bg)$arrow.mode <- 0
+#layout randomly
+plot(net.bg, layout=layout_randomly)
+#layout in circle
+l <- layout_in_circle(net.bg)
+plot(net.bg, layout=l)
+#layout in line
+l <- cbind(1:vcount(net.bg), c(1, vcount(net.bg):2))
+plot(net.bg, layout=l)
+#3D sphere layout
+l <- layout_randomly(net.bg)
+plot(net.bg, layout=l)
+#Force directed layout (Fruchterman-Reingold)
+l <- layout_with_fr(net.bg)
+x11()
+plot(net.bg, layout=l)
+#### CREO QUE FALTA UN METODO PARA DIBUJAR UN EDGE ENTRE DOS NODOS, 
+#### SOLO SI SU DISTANCIA ES MENOR A CIERTO VALOR
+g <- graph.full(nrow(family_distmx))
+V(g)$label <- rownames(family_distmx)
+layout <- layout_with_mds(g, dist = as.matrix(family_distmx))
+plot(g, layout = layout, vertex.size = 4)
 
 
-res.pca <- prcomp(datos, scale = TRUE) #make PCA
-groups <- as.factor(Pertenencia.df$Consenso) #recover clusters
-outfile4 <- paste(family,"_PCA_clusters.tiff", sep = "")
-#Draw PCA to tiff file
-tiff(outfile4, units="in", width=7, height=5, res=600) #tiff resolution parameters
-fviz_pca_ind(res.pca,
-             col.ind = groups, # color by groups
-             palette = c("#00AFBB", 
-                         "#FC4E07", 
-                         "#50f0d0", 
-                         "#f05070", 
-                         "#d050f0", 
-                         "#70f050", 
-                         "#f0d050", 
-                         "#ffd88d", 
-                         "#5086a7", 
-                         "#dd9082", 
-                         "#093d3a", 
-                         "#196f69", 
-                         "#786045", 
-                         "#d59c2c", 
-                         "#336936", 
-                         "#d050f0", 
-                         "#70f050", 
-                         "#f0d050", 
-                         "#50f0d0", 
-                         "#f05070"),
-             addEllipses = TRUE,
-             ellipse.level=0.95,
-             legend.title = "cluster",
-             label = "none",
-             title = paste(family, "clusters")
-)
-dev.off()
+### CREO QUE DEBO FILTRAR 
+
+myAdjacencyMatrix <- as.matrix(family_distmx)
+class(myAdjacencyMatrix)
+gra  <- graph.adjacency(myAdjacencyMatrix,weighted=TRUE)
+df <- get.data.frame(gra)
+head(df)
+
+d <- as.matrix(family_distmx)
+
+d_mas_uno <- as.dist(d+1)
+
+#Me quedé buscando una forma de pasar de matriz de distancias a matriz de similitud
+s <- ((1*d)-1)
+
+#FROM DISTANCE TO SIMMILARITY MATRIX
+length(family_distmx)
+
+family_simmx <- family_distmx
+for (i in 1:length(family_simmx)){
+  ds <- as.numeric(family_simmx[[i]])
+  #family_simmx[[i]] <- (ds * 200)
+  family_simmx[[i]] <- ifelse(ds==0,1,((1/ds)-1))
+}
+
+
